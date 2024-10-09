17852a9d9a0df515d230830948d5da77352d5771
haonq2001 <quanghao2001pt@gmail.com>
2024-10-09 15:14:00 +0700

    them chuyen dong nhan vat

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,18 +1,51 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
-{
+public class PlayerMove : MonoBehaviour { 
+     private int m_ColCount = 0;
+
+private float m_DisableTimer;
+
     // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        m_ColCount = 0;
     }
 
-    // Update is called once per frame
+    public bool State()
+    {
+        if (m_DisableTimer > 0)
+            return false;
+        return m_ColCount > 0;
+    }
+
+  void OnTriggerEnter2D(Collider2D other)
+{
+    // Kiểm tra nếu đối tượng va chạm có phải là lớp mặt đất hay không
+    if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    {
+        m_ColCount++;
+    }
+}
+
+void OnTriggerExit2D(Collider2D other)
+{
+    // Kiểm tra nếu đối tượng rời đi có phải là lớp mặt đất hay không
+    if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+    {
+        m_ColCount--;
+    }
+}
+
+
     void Update()
     {
-        
+        m_DisableTimer -= Time.deltaTime;
+    }
+
+    public void Disable(float duration)
+    {
+        m_DisableTimer = duration;
     }
 }
