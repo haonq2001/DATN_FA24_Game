c483b64cd1ae954da55363d24e5a0e2afada77f9
haonq2001 <quanghao2001pt@gmail.com>
2024-10-20 13:22:53 +0700

    OK

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     Rigidbody2D rb;
-    // Start is called before the first frame update
+    float speed = 5f;
+    Vector2 moveDirection; // Vector lưu trữ hướng di chuyển
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, 3f);
+
+        // Lấy hướng quay của nhân vật từ PlayerController
+        bool isFacingRight = PlayerController.facingRight;
+
+        // Thiết lập hướng di chuyển của viên đạn dựa trên hướng quay của nhân vật
+        if (isFacingRight)
+        {
+            moveDirection = Vector2.right;
+            rb.velocity = moveDirection * speed;
+            Debug.Log("Di chuyển sang phải");
+        }
+        else
+        {
+            moveDirection = Vector2.left;
+            rb.velocity = moveDirection * speed;
+            Debug.Log("Di chuyển sang trái");
+        }
     }
-  
-    // Update is called once per frame
-    void Update()
-    {
-        rb.velocity = new Vector2(-5f, rb.velocity.y);
-    }
+
+    // Các phương thức Update và huongdichuyen() khác
 }
