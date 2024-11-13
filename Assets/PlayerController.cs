3a6a426915bd890392052be936018a96312f0686
haonq2001 <quanghao2001pt@gmail.com>
2024-11-13 23:54:50 +0700

    Revert "Merge branch 'deft/hao' of https://github.com/haonq2001/DATN_FA24_Game into deft/hao"
    This reverts commit 6d207170b8c8530834bd73caabeff10189df262d, reversing
    changes made to 904fcfb3e007213cf2ac36c1158c95920441340d.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,22 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
 
 public class PlayerController : MonoBehaviour
 {
-    public GameManager gameManager;
-
-    public Slider playerHealth;
-    public Image fillImage;
-    public Slider playerMana;
-    public Image fillImagemana;
-
-
-
-
     public float moveSpeed = 2f;
     public float jumpForce = 2f;
     private Rigidbody2D rb;
@@ -30,12 +17,6 @@ public class PlayerController : MonoBehaviour
     private float lastCastTime = 0f;
     public GameObject swordCollider;
     public GameObject swordCollider1;
-    public int torchCount = 0;  // Biến để lưu trữ số ngọn lửa (hoặc đuốc) của người chơi
-    public int health = 10;
-    public int mana = 10;
-
-
-    public Image[] buttonImages; // Biến để tham chiếu đến Image của button
 
     void Start()
     {
@@ -45,66 +26,24 @@ public class PlayerController : MonoBehaviour
 
         swordCollider.SetActive(false);
         swordCollider1.SetActive(false);
-
-        playerHealth.maxValue = health;
-        playerHealth.value = health;
-        playerMana.maxValue = mana;
-        playerMana.value = mana;
-
     }
 
     void Update()
     {
         Move();
         Jump();
+        Cast();
         Attack();
-
+        Attack1();
 
         // Kiểm tra nhấn phím Space để bắn đạn
         //if (Input.GetKeyDown(KeyCode.Space))
         //{
         //    PlayerShoot();
         //}
-        if (playerMana.value > 0) {
-            Attack1();
-            Cast();
-            Attack3();
-        }
-        else
-        {
-            Debug.Log("Không đủ năng lượng để sử dụng kĩ năng");
-        }
-
-
-        // Kiểm tra mana và cập nhật tất cả các button
-        foreach (Image buttonImage in buttonImages)
-        {
-            UpdateButton(buttonImage);
-        }
     }
 
-
-
-
-    void UpdateButton(Image button)
-    {
-        if (playerMana.value <= 0)
-        {
-            Color color = button.color;  // Truy cập thuộc tính color của Image
-            color.a = 0.5f; // Làm mờ button
-            button.color = color;
-            button.GetComponent<Button>().interactable = false; // Tắt button
-        }
-        else
-        {
-            Color color = button.color;  // Truy cập thuộc tính color của Image
-            color.a = 1f; // Đặt lại độ sáng
-            button.color = color;
-            button.GetComponent<Button>().interactable = true; // Bật lại button
-        }
-    }
-
-        public void PlayerShoot()
+    public void PlayerShoot()
     {
         GameObject newBullet = Instantiate(bullet, bulletPos.position, Quaternion.identity);
         Bullet bulletScript = newBullet.GetComponent<Bullet>();
@@ -128,135 +67,18 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("attack");
         }
     }
-    public void buttonattack()
-    {
-        animator.SetTrigger("attack");
-    }
 
     void Attack1()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             animator.SetTrigger("strike");
-            playerMana.value-= 1;
-            
-        }
-       
-       
-    }
-    void Attack3()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            animator.SetTrigger("block");
-            playerMana.value -= 1;
-
         }
     }
-        public void buttonattack1()
-    {
-        if (playerMana.value > 0)
-        {
-            animator.SetTrigger("strike");
-            playerMana.value -= 1;
-        }
-        else
-        {
-            Debug.Log("Không đủ mana để sử dụng kĩ năng!");
-        }
-    }
-    public void buttonattack2()
-    {
-        if (playerMana.value > 0 && Time.time - lastCastTime >= castCooldown)
-        {
-            animator.SetTrigger("cast");
-            playerMana.value -= 2;
-            lastCastTime = Time.time;
-
-            // Làm mờ button và chạy cooldown
-            StartCoroutine(DisableButtonForCooldown(buttonImages[2]));
-        }
-        else
-        {
-          //  Debug.Log("Không đủ mana để sử dụng kĩ năng!");
-        }
-    }
-
-    IEnumerator DisableButtonForCooldown(Image button)
-    {
-        if (button == null)
-        {
-            Debug.LogWarning("Button image không được gán!");
-            yield break;
-        }
-
-        // Tìm Button component từ Image
-        Button buttonComponent = button.GetComponent<Button>();
-        if (buttonComponent == null)
-        {
-            Debug.LogWarning("Không tìm thấy Button component trên Image!");
-            yield break;
-        }
-
-        // Tắt button và làm mờ
-        Color color = button.color;
-        color.a = 0.5f;
-        button.color = color;
-        buttonComponent.interactable = false;
-
-        // Đợi 2 giây
-        yield return new WaitForSeconds(2f);
-
-        // Bật lại button và làm sáng
-        color.a = 1f;
-        button.color = color;
-        buttonComponent.interactable = true;
-
-        Debug.Log("Button đã trở lại bình thường sau 2 giây");
-    }
-
-    public void buttonattack3()
-    {
-        if (playerMana.value > 0)
-        {
-            animator.SetTrigger("block");
-            playerMana.value -= 1;
-        }
-        else
-        {
-            Debug.Log("Không đủ mana để sử dụng kĩ năng!");
-        }
-    }
-    private void UpdateSwordColliderPosition()
-    {
-        // Kiểm tra hướng nhân vật
-        if (facingRight)
-        {
-            // Nếu hướng phải, đặt swordCollider bên phải
-            swordCollider.transform.localPosition = new Vector3(0.2f, swordCollider.transform.localPosition.y, swordCollider.transform.localPosition.z);
-            swordCollider.transform.localScale = new Vector3(1, 1, 1); // Điều chỉnh hướng Box Collider cho đúng
-            swordCollider1.transform.localPosition = new Vector3(0.2f, swordCollider.transform.localPosition.y, swordCollider.transform.localPosition.z);
-            swordCollider1.transform.localScale = new Vector3(1, 1, 1); // Điều chỉnh hướng Box Collider cho đúng
-        }
-        else
-        {
-            // Nếu hướng trái, đặt swordCollider bên trái
-            swordCollider.transform.localPosition = new Vector3(-0.2f, swordCollider.transform.localPosition.y, swordCollider.transform.localPosition.z);
-            swordCollider.transform.localScale = new Vector3(-1, 1, 1); // Điều chỉnh hướng Box Collider cho đúng
-                                                                       // Nếu hướng trái, đặt swordCollider bên trái
-            swordCollider1.transform.localPosition = new Vector3(-0.2f, swordCollider.transform.localPosition.y, swordCollider.transform.localPosition.z);
-            swordCollider1.transform.localScale = new Vector3(-1, 1, 1); // Điều chỉnh hướng Box Collider cho đúng
-        }
-    }
-
 
     public void ShowSword()
     {
         swordCollider.SetActive(true);
-
-        UpdateSwordColliderPosition();
-        swordCollider1.SetActive(false);
-
     }
 
     public void HideSword()
@@ -266,8 +88,6 @@ public class PlayerController : MonoBehaviour
     public void ShowSword1()
     {
         swordCollider1.SetActive(true);
-        UpdateSwordColliderPosition();
-        swordCollider.SetActive(false);
     }
 
     public void HideSword1()
@@ -284,14 +104,12 @@ public class PlayerController : MonoBehaviour
         {
             spriteRenderer.flipX = true;
             facingRight = false;
-        UpdateSwordColliderPosition();
-    }
+        }
         else if (moveInput > 0)
         {
             spriteRenderer.flipX = false;
             facingRight = true;
-        UpdateSwordColliderPosition();
-    }
+        }
 
         animator.SetBool("isWalking", moveInput != 0);
         animator.SetBool("IsGround", isGrounded);
@@ -312,7 +130,6 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q) && Time.time - lastCastTime >= castCooldown)
         {
             animator.SetTrigger("cast");
-            playerMana.value -= 2;
             lastCastTime = Time.time;
         }
     }
@@ -346,20 +163,6 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("ngonlua"))
         {
             StartCoroutine(DestroyTorchAfterDelay(collision.gameObject));
-            gameManager.AddScore(); 
-            gameManager.SetScoreText();
-            torchCount++;
-            Debug.Log("Bạn vừa nhận được ngọn đuốc");
-            // Gọi hàm trong CotDuocManager để bật panel settings
-            CotDuocManager cotDuocManager = FindObjectOfType<CotDuocManager>();
-            if (cotDuocManager != null)
-            {
-                cotDuocManager.ActivatePanel();  // Bật panel settings
-            }
-        }
-        if (collision.gameObject.CompareTag("quaman"))
-        {
-            SceneManager.LoadSceneAsync(3);
         }
     }
 
@@ -370,7 +173,7 @@ public class PlayerController : MonoBehaviour
     }
     IEnumerator DestroyTorchAfterDelay(GameObject torch)
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(1f);
         Destroy(torch);
     }
 
